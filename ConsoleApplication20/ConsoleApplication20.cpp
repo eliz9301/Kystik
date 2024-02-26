125e272900f7c9825762e334271004dd05879ab3
bvzgjkz <122781449+bvzgjkz@users.noreply.github.com>
2024-02-27 05:53:47 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication20/ConsoleApplication20.cpp
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double number = 16.0;
+    double result = sqrt(number);
+    printf("Корень числа %.2f равен %.2f\n", number, result);
+    return 0;
+}
