c76a32ad49f0160f64168d2c7cf6564429a01ea1
bvzgjkz <122781449+bvzgjkz@users.noreply.github.com>
2024-02-27 05:54:51 +0600

    Задание готово к проверке

--- ConsoleApplication20/ConsoleApplication20.cpp
+++ ConsoleApplication20/ConsoleApplication20.cpp
@@ -5,5 +5,7 @@ int main() {
     double number = 16.0;
     double result = sqrt(number);
     printf("Корень числа %.2f равен %.2f\n", number, result);
+    result *= 2;
+    printf("Значение результат, умноженное на 2: %.2f", result);
     return 0;
 }
